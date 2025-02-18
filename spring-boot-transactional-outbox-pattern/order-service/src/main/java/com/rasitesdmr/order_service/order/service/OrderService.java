bfba99560020c25cffc6331c6fd0b35d8b4e2641
Muhammed Raşit Eşdemir <mrasitesdemir7@gmail.com>
2025-02-18 10:34:04 +0300

    Delete spring-boot-transactional-outbox-pattern directory

--- spring-boot-transactional-outbox-pattern/order-service/src/main/java/com/rasitesdmr/order_service/order/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.rasitesdmr.order_service.order.service;
-
-import com.rasitesdmr.order_service.order.Order;
-import com.rasitesdmr.order_service.order.model.request.OrderCreateRequest;
-
-public interface OrderService {
-
-    Order createOrder(OrderCreateRequest orderCreateRequest);
-}
--- spring-boot-transactional-outbox-pattern/order-service/src/main/java/com/rasitesdmr/order_service/outbox/service/OutboxService.java
+++
@@ -1,9 +0,0 @@
-package com.rasitesdmr.order_service.outbox.service;
-
-import com.rasitesdmr.order_service.order.Order;
-import com.rasitesdmr.order_service.outbox.Outbox;
-
-public interface OutboxService {
-
-    void createOutbox(Order order);
-}
