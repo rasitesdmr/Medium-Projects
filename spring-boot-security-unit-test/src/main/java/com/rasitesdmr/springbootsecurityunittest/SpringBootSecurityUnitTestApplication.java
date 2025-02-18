d78eb50e9ff69c73b1d849b9382b60c2c120d85d
Raşit Eşdemir <rasit.esdemir@detaysoft.com>
2025-02-18 10:36:42 +0300

    feat : updated project

---
+++ spring-boot-security-unit-test/src/main/java/com/rasitesdmr/springbootsecurityunittest/SpringBootSecurityUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.rasitesdmr.springbootsecurityunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityUnitTestApplication.class, args);
+    }
+
+}
---
+++ spring-boot-security-unit-test/src/test/java/com/rasitesdmr/springbootsecurityunittest/SpringBootSecurityUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasitesdmr.springbootsecurityunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
