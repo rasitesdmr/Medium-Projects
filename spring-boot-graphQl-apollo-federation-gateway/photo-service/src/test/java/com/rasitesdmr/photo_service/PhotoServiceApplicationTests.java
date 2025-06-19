95c89797065cbf7056f1e7840ce67fa92f747b7d
rasitesdmr <mrasitesdemir7@gmail.com>
2025-06-19 14:06:32 +0300

    feat: created photo service

---
+++ spring-boot-graphQl-apollo-federation-gateway/photo-service/src/test/java/com/rasitesdmr/photo_service/PhotoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasitesdmr.photo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
