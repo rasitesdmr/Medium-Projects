d6eda0f2cfab64ba46c45396e6bbfbb1cf0bf227
rasitesdmr <mrasitesdemir7@gmail.com>
2025-06-26 12:11:04 +0300

    feat: user response and auditable date field updated

--- spring-boot-graphQl-apollo-federation-gateway/user-service/src/main/java/com/rasitesdmr/user_service/domain/response/UserResponse.java
+++ spring-boot-graphQl-apollo-federation-gateway/user-service/src/main/java/com/rasitesdmr/user_service/domain/response/UserResponse.java
@@ -2,6 +2,8 @@ package com.rasitesdmr.user_service.domain.response;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Getter
@@ -13,10 +15,10 @@ public class UserResponse {
      private String firstName;
      private String lastName;
      private String email;
-     private Date createDate;
-     private Date updateDate;
+     private LocalDateTime  createDate;
+     private LocalDateTime  updateDate;
 
-     public UserResponse(Long id, String firstName, String lastName, String email, Date createDate, Date updateDate) {
+     public UserResponse(Long id, String firstName, String lastName, String email, LocalDateTime  createDate, LocalDateTime  updateDate) {
           this.id = id;
           this.firstName = firstName;
           this.lastName = lastName;
